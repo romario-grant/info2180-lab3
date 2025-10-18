681052ab33f1955487e829915c2a79c3b18094b7
Romario Grant <romario10grant@gmail.com>
2025-10-17 22:30:56 -0500

    Added initial starter files

--- tic-tac-toe.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const gameBoard = document.getElementById('board');
-    
-    const squares = gameBoard.querySelectorAll('div');
-    
-    squares.forEach(square => {
-        square.classList.add('square');
-    });
-});
